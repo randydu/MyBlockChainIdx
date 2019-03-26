1d3c74a32b8830678075494744124a96d033ff2b
randy <randydu@gmail.com>
2019-03-25 17:20:55 -0700

    + debug_mem_leak_level;

--- changelog.md
+++ changelog.md
@@ -4,6 +4,12 @@ Change Log
 v1.3.2 (Jan. 22, 2019)
 ----------------------
 
+- refactorize the sampling loop to avoid possible memory leakage;
+- adds memory leakage detection logic; (DEBUG_MEM_LEAK=0*,1,2)
+  * 0: no detection;
+  * 1: leak+stats;
+  * 2: leak+stats+heap_diff;
+
 
 
 v1.3.1 (Dec. 31, 2018)
--- index.js
+++ index.js
@@ -2,12 +2,16 @@
 
 require('dotenv').config({ path: __dirname + '/.env'});
 
+var memwatch = null;
+const debug_mem_leak_level = +process.env.DEBUG_MEM_LEAK;
+
 const common = require('./common');
 const debug = require('mydbg')('index');
 
 const sample = require('./sam');
 const dal = require('./dal');
 const api = require('./api');
+
 
 let quit = false;
 
@@ -32,39 +36,58 @@ process.on('SIGTERM', handle);
 const sample_interval = +process.env.SAMPLE_INTERVAL || 1000;
 console.log(`Sampling interval = ${sample_interval}`);
 
-function sample_run(){
-    if(quit) return Promise.resolve(-1);
-
-    return sample.run().then(r => {
-        if(quit) return Promise.resolve(-1);
-
-        return common.delay(sample_interval).then(sample_run);
-        //setTimeout(sample_run, sample_interval);
-    })
-}
-
 
 async function init(){
     await dal.init();
     await sample.init();
+
+    //leakage detection
+    //# 0: no detection 1: leak+stats 2: leak+stats+heap_diff
+    if(debug_mem_leak_level > 0){
+        memwatch = require('node-memwatch');
+        
+        memwatch.on('leak', function(info){
+            debug.warn('%O', info);
+        });
+
+        memwatch.on('stats', function(stats){
+            debug.info('%O', stats);
+        });
+    }
 }
 
 async function do_sample(){
-    await init();
     
     try {
+        await init();
+
+        let to_mem_leak_diff = debug_mem_leak_level == 2;
+
         while(!quit){
+            let hd = to_mem_leak_diff ? new memwatch.HeapDiff() : null;
+
             await sample.run();
             if(!quit) await common.delay(sample_interval);
+
+            if(hd != null){
+                let diff = hd.end();
+                debug.info('%O', diff);
+            }
         }
     }catch(err){
         debug.err(err.message);
 
-        await dal.logEvent({
-            message: err.message,
-            code: 'ERROR',
-            level: dal.LOG_LEVEL_ERROR
-        });
+        try {
+            debug.info('saving err message to database...');
+
+            await dal.logEvent({
+                message: err.message,
+                code: 'ERROR',
+                level: dal.LOG_LEVEL_ERROR
+            });
+        }catch(ex){
+            debug.err(ex);
+        }
         
         process.exitCode = -1;
         
@@ -86,6 +109,17 @@ if(+process.env.HTTP){
 }
 
 /*
+function sample_run(){
+    if(quit) return Promise.resolve(-1);
+
+    return sample.run().then(r => {
+        if(quit) return Promise.resolve(-1);
+
+        return common.delay(sample_interval).then(sample_run);
+        //setTimeout(sample_run, sample_interval);
+    })
+}
+
 return init().then( sample_run )
     .catch(err => {
         debug.err(err.message);
